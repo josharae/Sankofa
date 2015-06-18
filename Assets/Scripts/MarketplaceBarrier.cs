7cc6b02f75413877061026d340788f8dc98f228f
Gavin Dent <gavindent@hotmail.com>
2015-06-17 20:05:23 -0700

    Invisible barrier for marketplace scene

---
+++ Assets/Scripts/MarketplaceBarrier.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarketplaceBarrier : MonoBehaviour {
+
+	public bool hasObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.CompareTag("MarketBarrier"))
+		{
+			if (this.hasObject){
+				SetAllWithTagInactive("MarketBarrier");
+			}
+		}
+	}
+	private void SetAllWithTagInactive(string tag)
+	{
+		GameObject[] barriers = GameObject.FindGameObjectsWithTag(tag);
+		for (int i = 0; i < barriers.Length; i++) {
+			barriers[i].gameObject.SetActive (false);
+		}
+	}
+}
