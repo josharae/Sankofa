e83b2bd136583901e55378788a9c08cd07419c55
gfazenda <gludvigf@uci.edu>
2015-06-17 20:35:49 -0700

    Player and Object script

---
+++ Assets/Scripts/ObjectScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectScript: MonoBehaviour {
+	GameObject player;
+	public bool isCollected = false;
+	Vector3 OriginalPosition;
+
+	void Start () {
+		player = GameObject.Find ("Player");
+		OriginalPosition = this.transform.position;
+	}
+
+	void OnMouseDown(){
+		if (Vector3.Distance (transform.position, player.transform.position) < 20 && !isCollected){
+			player.GetComponent<PlayerScript>().getObject(this.gameObject);
+		}
+	}
+
+	public void TeleportBack(){
+		this.transform.position = OriginalPosition;
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	private float speed = 2000f;
+	public GameObject camera;
+	public GameObject Hand;
+	int DownLimit = 50, UpLimit = -30;
+	GameObject Bone;
+	float yRotation = 0f, xRotation = 0f;
+	public bool hasObj = false;
+
+	void Start () {
+	}
+
+	void FixedUpdate () {
+			if (hasObj) //move bone with player
+				Bone.transform.position = Hand.transform.position;
+
+			//rotation
+			float mouseHorizontal = Input.GetAxis ("Mouse X");
+			float mouseVertical = Input.GetAxis ("Mouse Y");
+			yRotation += mouseHorizontal;
+			xRotation += mouseVertical * -1;
+			transform.eulerAngles = new Vector3 (0, yRotation, 0);
+
+			if (xRotation <= DownLimit && xRotation >= UpLimit) {
+				camera.transform.eulerAngles = new Vector3 (xRotation, yRotation, 0);
+			} else {
+				if (xRotation > DownLimit)
+					xRotation = DownLimit;
+				if (xRotation < UpLimit)
+					xRotation = UpLimit;
+			}
+
+			//movement
+			float moveVertical = Input.GetAxis ("Vertical");
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+			GetComponent<Rigidbody> ().velocity = Vector3.zero;
+
+			GetComponent<Rigidbody> ().AddRelativeForce (movement * speed);
+
+	}
+
+	void Update(){
+		if (Input.GetMouseButtonDown(1) && hasObj) {
+			ChangeBoneRigidBody (true);
+			hasObj = false;
+		}
+	}
+
+	public void ChangeBoneRigidBody(bool Active = false){
+		Bone.GetComponent<Rigidbody> ().useGravity = Active;
+		Bone.GetComponent<Rigidbody> ().detectCollisions = Active;
+	}
+
+	public void getObject(GameObject bone){
+        if (!hasObj)
+        {
+            hasObj = true;
+            Bone = bone;
+            ChangeBoneRigidBody();
+        }
+	}
+}
