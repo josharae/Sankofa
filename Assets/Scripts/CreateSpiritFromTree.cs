a60f41855a8870c0c72d5b7a8a2f3a3e67b750f2
Gavin Dent <gavindent@hotmail.com>
2015-06-17 18:43:02 -0700

    CreateSpiritFromTree.cs made

---
+++ Assets/Scripts/CreateSpiritFromTree.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateSpiritFromTree : MonoBehaviour {
+
+	public GameObject spirit;
+
+	// Use this for initialization
+	void Start () {
+		spirit.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Rock"))
+		{
+			spirit.gameObject.SetActive (true);
+		}
+	}
+}
