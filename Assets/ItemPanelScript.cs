03eff7adba196fcfb5781da28497b8fbb87b257a
gfazenda <gludvigf@uci.edu>
2015-06-22 20:53:00 -0700

    item popup

---
+++ Assets/ItemPanelScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ItemPanelScript : MonoBehaviour {
+	public GameObject Panel;
+	public Image ItemSprite;
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	public void ShowItemPanel (GameObject Item) {
+		Panel.gameObject.SetActive (true);
+		Sprite newSprite = Resources.Load <Sprite> (Item.name);
+		Debug.Log (Item.name);
+		ItemSprite.overrideSprite = (Sprite) newSprite;
+		Panel.GetComponentInChildren<Text> ().text = Item.name;
+	}
+}
