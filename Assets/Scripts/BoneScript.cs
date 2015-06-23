c846a0ce994a9368048537bb76188d519e83524a
Gavin Dent <gavindent@hotmail.com>
2015-06-22 20:06:48 -0700

    Changes to EarthSpiritAction.cs

---
+++ Assets/Scripts/BoneScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class BoneScript : MonoBehaviour {
+	GameObject player;
+	public bool isCollected = false;
+	Vector3 OriginalPosition;
+	void Start () {
+		player = GameObject.Find ("Player");
+		OriginalPosition = this.transform.position;
+	}
+
+	void OnMouseDown(){
+		if (Vector3.Distance (transform.position, player.transform.position) < 20 && !isCollected){
+			player.GetComponent<PlayerScript>().getObject(this.gameObject);
+		}
+	}
+
+
+
+	public void TeleportBack(){
+		this.transform.position = OriginalPosition;
+	}
+}
