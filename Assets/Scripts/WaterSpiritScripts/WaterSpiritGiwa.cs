239a806ba99fa1e88be881ce9c83925b08866b22
Gavin Dent <gavindent@hotmail.com>
2015-06-23 12:47:11 -0700

    WaterSpiritGiwa.cs may be done but cannot be completed until animation of Giwa waking up is complete.

---
+++ Assets/Scripts/WaterSpiritScripts/WaterSpiritGiwa.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterSpiritGiwa : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Giwa")) {
+			Animation anim = other.GetComponent<Animation>();
+			anim.Play ("Take 001", PlayMode.StopSameLayer);
+		}
+	}
+}
