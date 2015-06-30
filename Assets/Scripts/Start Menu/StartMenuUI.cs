d765e4eb3c1526be8bdb9142ae3ae79055a36fc0
mattwimer <jiraiu@yahoo.com>
2015-06-29 17:01:11 -0700

    Third Commit

---
+++ Assets/Scripts/Start Menu/StartMenuUI.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenuUI : MonoBehaviour {
+
+	void ChangeToScene(int sceneToChangeTo) {
+		Application.LoadLevel (sceneToChangeTo);
+	}
+}
