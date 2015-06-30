8bf1952f599aaad8a09b8c74f2d5921c92b82ea4
mattwimer <jiraiu@yahoo.com>
2015-06-29 20:55:56 -0700

    Fourth Commit
    6/29/15

---
+++ Assets/Scripts/Start Menu/ScriptyScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScriptyScript : MonoBehaviour {
+
+	public string name;
+	public InputField test;
+
+	public string display()
+	{
+		return test.textComponent.text;
+	}
+}
