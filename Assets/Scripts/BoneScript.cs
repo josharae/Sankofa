327872b71ff805fd745e27ced78e1e11afc3fbcf
rishitarora <rishitarora3@rocketmail.com>
2015-07-01 20:39:03 -0700

    Adjusted Throw/Pick Up Script, added audio to it.

--- Assets/Scripts/BoneScript.cs
+++ Assets/Scripts/BoneScript.cs
@@ -1,4 +1,30 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
+//
+//using System.Collections;
+//
+//public class BoneScript : MonoBehaviour {
+//	GameObject player;
+//	public bool isCollected = false;
+//	Vector3 OriginalPosition;
+//	void Start () {
+//		player = GameObject.Find ("Player");
+//		OriginalPosition = this.transform.position;
+//	}
+//
+//	void OnMouseDown(){
+//		if (Vector3.Distance (transform.position, player.transform.position) < 20 && !isCollected){
+//			player.GetComponent<PlayerScript>().getObject(this.gameObject);
+//		}
+//	}
+//
+//
+//
+//	public void TeleportBack(){
+//		this.transform.position = OriginalPosition;
+//	}
+//}
+
+using UnityEngine;
 
 using System.Collections;
 
@@ -6,20 +32,40 @@ public class BoneScript : MonoBehaviour {
 	GameObject player;
 	public bool isCollected = false;
 	Vector3 OriginalPosition;
+	public Animator Explosion;
+	private bool hasBeenThrown;
+	public AudioClip audioClip;
+	private AudioSource audio;
+
 	void Start () {
 		player = GameObject.Find ("Player");
 		OriginalPosition = this.transform.position;
+		Explosion = GetComponent<Animator> ();
+		hasBeenThrown = false;
+		Explosion.SetBool("Explode", false);
 	}
-
+	
 	void OnMouseDown(){
 		if (Vector3.Distance (transform.position, player.transform.position) < 20 && !isCollected){
 			player.GetComponent<PlayerScript>().getObject(this.gameObject);
 		}
 	}
+	
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.CompareTag ("Ground") && hasBeenThrown) {
+			Explosion.SetBool("Explode", true);
+			audio.PlayOneShot(audioClip);
 
-
-
+		}
+	}
+	
+	
 	public void TeleportBack(){
 		this.transform.position = OriginalPosition;
 	}
+	
+	public void SetThrownBool(bool thrown){
+		hasBeenThrown = thrown;
+	}
 }
+
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,49 +1,140 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class PlayerScript : MonoBehaviour {
+//	private float speed = 15f;
+//	public GameObject camera, Hand;
+//	GameObject GameManager;
+//	int DownLimit = 50, UpLimit = -30;
+//	GameObject Item;
+//	float yRotation = 0f, xRotation = 0f;
+//	public bool hasObj = false;
+//
+//	void Start () {
+//		GameManager = GameObject.Find ("GameManager");
+//	}
+//
+//	void FixedUpdate () {
+//			if (hasObj) //move bone with player
+//				Item.transform.position = Hand.transform.position;
+//
+////			//rotation
+////			float mouseHorizontal = Input.GetAxis ("Mouse X");
+////			float mouseVertical = Input.GetAxis ("Mouse Y");
+////			yRotation += mouseHorizontal;
+////			xRotation += mouseVertical * -1;
+////			transform.eulerAngles = new Vector3 (0, yRotation, 0);
+////
+////			if (xRotation <= DownLimit && xRotation >= UpLimit) {
+////				camera.transform.eulerAngles = new Vector3 (xRotation, yRotation, 0);
+////			} else {
+////				if (xRotation > DownLimit)
+////					xRotation = DownLimit;
+////				if (xRotation < UpLimit)
+////					xRotation = UpLimit;
+////			}
+//
+//			//movement
+////			float moveVertical = Input.GetAxis ("Vertical");
+////			float moveHorizontal = Input.GetAxis ("Horizontal");
+////			
+////			Vector3 movement = transform.forward * moveVertical * speed;
+////			movement.y = GetComponent<Rigidbody> ().velocity.y;
+////			GetComponent<Rigidbody> ().velocity =  (movement);
+//
+//	}
+//
+//	void Update(){
+//		if (Input.GetMouseButtonDown(1) && hasObj) {
+//			ChangeBoneRigidBody (true);
+//			hasObj = false;
+//		}
+//		if (Input.GetKeyDown (KeyCode.Space) && hasObj) {
+//			Throw ();
+//		}
+//	}
+//
+//	public void ChangeBoneRigidBody(bool Active){
+//		Active = false;
+//		Item.GetComponent<Rigidbody> ().useGravity = Active;
+//		Item.GetComponent<Rigidbody> ().detectCollisions = Active;
+//	}
+//
+//	public void getObject(GameObject newItem){
+//        if (!hasObj)
+//        {
+//			if(newItem.tag == Tags.Collectible){
+//				GameManager.GetComponent<ItemPanelScript>().ShowItemPanel(newItem);
+//				this.GetComponent<InventoryScript>().AddItem(newItem);
+//			}
+//			else {
+//	            hasObj = true;
+//				Item = newItem;
+//	            ChangeBoneRigidBody(false);
+//			}
+//        }
+//	}
+//
+//	public GameObject getObject(){
+//		return Item;
+//	}
+//
+//	void Throw()
+//	{
+//		hasObj = false;
+//		Item.GetComponent<Rigidbody>().AddRelativeForce (this.transform.forward * speed);
+//	}
+//}
+
+
+using UnityEngine;
 using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
-	private float speed = 15f;
+	private float speed = 2000f;
 	public GameObject camera, Hand;
 	GameObject GameManager;
 	int DownLimit = 50, UpLimit = -30;
 	GameObject Item;
 	float yRotation = 0f, xRotation = 0f;
 	public bool hasObj = false;
-
+	
+	
 	void Start () {
 		GameManager = GameObject.Find ("GameManager");
 	}
-
+	
 	void FixedUpdate () {
-			if (hasObj) //move bone with player
-				Item.transform.position = Hand.transform.position;
-
-			//rotation
-			float mouseHorizontal = Input.GetAxis ("Mouse X");
-			float mouseVertical = Input.GetAxis ("Mouse Y");
-			yRotation += mouseHorizontal;
-			xRotation += mouseVertical * -1;
-			transform.eulerAngles = new Vector3 (0, yRotation, 0);
-
-			if (xRotation <= DownLimit && xRotation >= UpLimit) {
-				camera.transform.eulerAngles = new Vector3 (xRotation, yRotation, 0);
-			} else {
-				if (xRotation > DownLimit)
-					xRotation = DownLimit;
-				if (xRotation < UpLimit)
-					xRotation = UpLimit;
-			}
-
-			//movement
-			float moveVertical = Input.GetAxis ("Vertical");
-			float moveHorizontal = Input.GetAxis ("Horizontal");
-			
-			Vector3 movement = transform.forward * moveVertical * speed;
-			movement.y = GetComponent<Rigidbody> ().velocity.y;
-			GetComponent<Rigidbody> ().velocity =  (movement);
-
+		if (hasObj) //move bone with player
+			Item.transform.position = Hand.transform.position;
+		
+//		//rotation
+//		float mouseHorizontal = Input.GetAxis ("Mouse X");
+//		float mouseVertical = Input.GetAxis ("Mouse Y");
+//		yRotation += mouseHorizontal;
+//		xRotation += mouseVertical * -1;
+//		transform.eulerAngles = new Vector3 (0, yRotation, 0);
+//		
+//		if (xRotation <= DownLimit && xRotation >= UpLimit) {
+//			camera.transform.eulerAngles = new Vector3 (xRotation, yRotation, 0);
+//		} else {
+//			if (xRotation > DownLimit)
+//				xRotation = DownLimit;
+//			if (xRotation < UpLimit)
+//				xRotation = UpLimit;
+//		}
+//		
+//		//movement
+//		float moveVertical = Input.GetAxis ("Vertical");
+//		float moveHorizontal = Input.GetAxis ("Horizontal");
+//		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+//		
+//		GetComponent<Rigidbody> ().velocity = Vector3.zero;
+//		
+//		GetComponent<Rigidbody> ().AddRelativeForce (movement * speed);
+//		
 	}
-
+	
 	void Update(){
 		if (Input.GetMouseButtonDown(1) && hasObj) {
 			ChangeBoneRigidBody (true);
@@ -53,35 +144,39 @@ public class PlayerScript : MonoBehaviour {
 			Throw ();
 		}
 	}
-
+	
 	public void ChangeBoneRigidBody(bool Active){
-		Active = false;
+		Item.GetComponent<Rigidbody> ().velocity = Vector3.zero;
 		Item.GetComponent<Rigidbody> ().useGravity = Active;
 		Item.GetComponent<Rigidbody> ().detectCollisions = Active;
 	}
-
+	
 	public void getObject(GameObject newItem){
-        if (!hasObj)
-        {
+		if (!hasObj)
+		{
 			if(newItem.tag == Tags.Collectible){
 				GameManager.GetComponent<ItemPanelScript>().ShowItemPanel(newItem);
 				this.GetComponent<InventoryScript>().AddItem(newItem);
 			}
 			else {
-	            hasObj = true;
+				hasObj = true;
 				Item = newItem;
-	            ChangeBoneRigidBody(false);
+				ChangeBoneRigidBody(false);
 			}
-        }
+		}
 	}
-
+	
 	public GameObject getObject(){
 		return Item;
 	}
-
+	
 	void Throw()
 	{
 		hasObj = false;
-		Item.GetComponent<Rigidbody>().AddRelativeForce (this.transform.forward * speed);
+		ChangeBoneRigidBody(true);
+		Item.GetComponent<Rigidbody>().AddRelativeForce (this.transform.forward * 1000);
+		BoneScript bs = Item.GetComponent<BoneScript> ();
+		bs.SetThrownBool (true);
 	}
 }
+
