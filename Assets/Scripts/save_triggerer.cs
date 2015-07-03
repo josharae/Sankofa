2fb85d272b57f6091c8116953809a08b45d607c7
Gavin Dent <gavindent@hotmail.com>
2015-07-02 20:15:37 -0700

    Final Oware sync for 7/15

--- Assets/Scripts/save_triggerer.cs
+++ Assets/Scripts/save_triggerer.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class save_triggerer : MonoBehaviour {
 
 	void OnTriggerEnter (Collider other) {
-		if(other.gameObject.CompareTag("Player"){
+		if(other.gameObject.CompareTag("Player")){
 			//saveGame(); // could be a static method
 			Debug.Log ("Saved Game");
 		}
