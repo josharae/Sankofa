2921eeb21febf8d11fa7412ff519ec913d380a2d
mattwimer <jiraiu@yahoo.com>
2015-07-02 18:19:00 -0700

    hi

---
+++ Assets/Scripts/save_triggerer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class save_triggerer : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other) {
+		if(other.gameObject.CompareTag("Player"){
+			//saveGame(); // could be a static method
+			Debug.Log ("Saved Game");
+		}
+	}
+	
+	
+}
