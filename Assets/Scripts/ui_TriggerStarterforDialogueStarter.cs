f76578c6f954e9468147ca44f7d3bf41ac79676c
Shivam-Shah <mathwizard98@gmail.com>
2015-07-07 19:32:32 -0700

    Changing the name of the dialogues to make fore sense and compiles in monoDevelop

---
+++ Assets/Scripts/ui_TriggerStarterforDialogueStarter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ui_TriggerStarterforDialogueStarter : MonoBehaviour {
+
+	public bool grow = false;
+	public float timer = 0.0f;
+
+	private Vector3 origScale;
+	private Vector3 targScale;
+		
+	void Update () {
+
+		if(grow)
+		{
+			timer += Time.deltaTime;
+			transform.localScale = Vector3.Lerp(origScale, targScale, timer);
+		}
+
+		if(timer >= 1)
+		{
+			grow = false;
+		}
+	}
+
+	public void Grow () {
+
+		grow = true;
+		origScale = transform.localScale;
+		targScale = origScale + new Vector3(1, 1, 1);
+	}
+}
