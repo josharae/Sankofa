1920a8d924e67117b997682969885147db13ec83
gfazenda <gludvigf@uci.edu>
2015-07-07 20:40:15 -0700

    new giwa commit

--- Assets/Scripts/Phase2_script_GiwaAttack.cs
+++ Assets/Scripts/Phase2_script_GiwaAttack.cs
@@ -5,10 +5,11 @@ public class Phase2_script_GiwaAttack : MonoBehaviour
 {
 	GameObject player;
 	public GameObject arenaPosition;
-	private float MaxSpeed = 2500;
+	private float MaxSpeed = 30;
 	private bool isStunned = false, isSleeping = true, isChasing = false;
-	Vector3 OriginalPosition;
-
+	Vector3 originalPosition;
+	Quaternion originalRotation;
+	float currentRate, initialRate = 0.02f, chargingTime = 5f;
 //	private void slowing()
 //	{
 //		Rigidbody rb = GetComponent<Rigidbody> ();
@@ -20,16 +21,17 @@ public class Phase2_script_GiwaAttack : MonoBehaviour
 	void Start()
 	{
 		player = GameObject.Find ("Player");
-		OriginalPosition = this.transform.position;
+		originalPosition = this.transform.position;
+		originalRotation = this.transform.rotation;
+		currentRate = initialRate;
 		//Target = new GameObject();
 		//Target.transform.position.Set (Player.transform.position.x,Player.transform.position.y,Player.transform.position.z);
 	}
 
-	void Update()
+	void FixedUpdate()
 	{
 		//Target.transform.position.Set(Player.transform.position.x,Player.transform.position.y,Player.transform.position.z);
 		//slowing ();
-		Rigidbody rb = GetComponent<Rigidbody> ();
 		//if (rb.velocity <= (new Vector3 (25, 25, 25)))
 		//	charge = true;
 //		if (isSlowing) // WHAT IS THE SLOWING CONDITION
@@ -40,24 +42,54 @@ public class Phase2_script_GiwaAttack : MonoBehaviour
 //		}
 		if(Vector3.Distance(this.transform.position,player.transform.position) < 150)
 		{
-			transform.LookAt(player.transform);
-			if(!isChasing) Invoke ("charge",5f);
-			else if(isChasing) this.GetComponent<Rigidbody>().AddRelativeForce  (Vector3.forward * MaxSpeed);
+			if(!isChasing){
+				chargingTime = 5f;
+				if(isStunned){
+					chargingTime += 3f;
+					isStunned = false;
+				}
+				Invoke ("charge",chargingTime);
+			}
+			else if(isChasing){
+				//transform.LookAt(player.transform);
+				Vector3 target = new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z);
+				transform.LookAt(target);
+				currentRate+=Time.deltaTime;
+				float vel = Mathf.Lerp(0,MaxSpeed,currentRate);
+				Vector3 movement = transform.forward * vel;
+				Debug.Log(vel);
+				movement.y = GetComponent<Rigidbody> ().velocity.y;
+				//GetComponent<Rigidbody> ().velocity =  movement;
+				transform.Translate(MaxSpeed*Vector3.forward*Time.deltaTime);
+			}
+			//	this.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * MaxSpeed);
 		}
 	}
 
 	void charge()
 	{
 		isChasing = true;
+		CancelInvoke ();
+	}
 
+	void resetPosition(){
+		this.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+		this.GetComponent<Rigidbody> ().angularVelocity = Vector3.zero;
+		this.transform.position = arenaPosition.transform.position;
+		this.transform.rotation = originalRotation;
+		currentRate = initialRate;
+		isChasing = false;
 	}
 
 	void OnCollisionEnter(Collision other){
-		if (other.gameObject.tag == Tags.Boulder)
+		if (other.gameObject.tag == Tags.Boulder || other.gameObject.tag == Tags.Player) {
 			isStunned = true;
+			resetPosition();
+		}
+
 		else if (other.gameObject.tag == Tags.WaterSpirit) {
 			isSleeping = false;
-			this.transform.position = arenaPosition.transform.position;
+
 		}
 	}
 }
