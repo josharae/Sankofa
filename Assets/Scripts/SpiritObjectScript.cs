d13846274f9472e706bef38c394bc9f225c4bab3
peterding1 <peterding@dhcp-v050-210.mobile.uci.edu>
2015-07-09 14:17:56 -0700

    spiritscript

--- Assets/Scripts/SpiritObjectScript.cs
+++ Assets/Scripts/SpiritObjectScript.cs
@@ -10,18 +10,17 @@ public class SpiritObjectScript : MonoBehaviour {
 
 	void Start () {
 		player = GameObject.Find ("Player");
-		//OriginalPosition = this.transform.position;
 		Glow = GetComponent<Animator> ();
 		isGlowing = true;
 		Glow.SetBool("Glow", true);
 	}
 	void FixedUpdate(){
-		Glow.SetBool("Glow", isGlowing);
+		//Glow.SetBool("Glow", isGlowing);
 	}
 	
 	void OnMouseDown(){
 		if (Vector3.Distance (transform.position, player.transform.position) < 20 && isGlowing){
-			player.GetComponent<PlayerScript2>().getObject(spirit.gameObject);
+			player.GetComponent<PlayerScript>().getObject(spirit.gameObject);
 		}
 	}
 }
---
+++ Assets/Scripts/SpiritScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class SpiritScript : MonoBehaviour {
+	GameObject player;
+	public bool isCollected = false;
+	Vector3 OriginalPosition;
+	public Animator Explosion;
+	private bool hasBeenThrown;
+	private AudioSource audio;
+	
+	void Start () {
+		player = GameObject.Find ("Player");
+		OriginalPosition = this.transform.position;
+		Explosion = GetComponent<Animator> ();
+		hasBeenThrown = false;
+		Explosion.SetBool("Explode", false);
+	}
+	
+	void OnMouseDown(){
+		if (Vector3.Distance (transform.position, player.transform.position) < 20 && !isCollected){
+			player.GetComponent<PlayerScript>().getObject(this.gameObject);
+		}
+	}
+	
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.CompareTag ("Ground") && hasBeenThrown) {
+			this.GetComponent<AudioSource>().Play();
+			Explosion.SetBool("Explode", true);
+			
+			
+		}
+	}
+	
+	
+	public void TeleportBack(){
+		this.transform.position = OriginalPosition;
+	}
+	
+	public void SetThrownBool(bool thrown){
+		hasBeenThrown = thrown;
+	}
+}
