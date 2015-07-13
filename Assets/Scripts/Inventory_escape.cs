2408e5dc0965cc76c34defb07f68780ca1154654
gfazenda <gludvigf@uci.edu>
2015-07-13 16:10:43 -0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Assets/_Scenes/InventoryScene.unity
    
    first build

---
+++ Assets/Scripts/Inventory_escape.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory_escape : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.LoadLevel(Scenes.MainScene);
+		}
+	}
+}
