3287d76a1133323122c73f4f5c393bf34a056842
gfazenda <gludvigf@uci.edu>
2015-07-13 19:42:24 -0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Assets/Resources/Dialogues/subtitle work no.docx.meta
    	Assets/Resources/Dialogues/subtitle work yes.docx.meta
    	Assets/Standard Assets/ParticleSystems/Prefabs/Afterburner.prefab
    	Assets/Standard Assets/ParticleSystems/Prefabs/Afterburner.prefab.meta
    	Assets/Standard Assets/ParticleSystems/Prefabs/DustStorm.prefab
    	Assets/Standard Assets/ParticleSystems/Prefabs/DustStorm.prefab.meta
    	Assets/Standard Assets/ParticleSystems/Prefabs/DustStormMobile.prefab
    	Assets/Standard
    Assets/ParticleSystems/Prefabs/DustStormMobile.prefab.meta
    	Assets/Standard Assets/ParticleSystems/Prefabs/Explosion.prefab
    	Assets/Standard Assets/ParticleSystems/Prefabs/Explosion.prefab.meta
    	Assets/Standard Assets/ParticleSystems/Prefabs/ExplosionMobile.prefab
    	Assets/Standard
    Assets/ParticleSystems/Prefabs/ExplosionMobile.prefab.meta
    	Assets/Standard Assets/ParticleSystems/Prefabs/FireComplex.prefab
    	Assets/Standard Assets/ParticleSystems/Prefabs/FireComplex.prefab.meta
    	Assets/Standard Assets/ParticleSystems/Prefabs/FireMobile.prefab
    	Assets/Standard Assets/ParticleSystems/Prefabs/FireMobile.prefab.meta
    	Assets/Standard Assets/ParticleSystems/Prefabs/Fireworks.prefab
    	Assets/Standard Assets/ParticleSystems/Prefabs/Fireworks.prefab.meta
    	Assets/Standard Assets/ParticleSystems/Prefabs/Flare.prefab
    	Assets/Standard Assets/ParticleSystems/Prefabs/Flare.prefab.meta
    	Assets/Standard Assets/ParticleSystems/Prefabs/FlareMobile.prefab
    	Assets/Standard Assets/ParticleSystems/Prefabs/FlareMobile.prefab.meta
    	Assets/Standard Assets/ParticleSystems/Prefabs/Hose.prefab
    	Assets/Standard Assets/ParticleSystems/Prefabs/Hose.prefab.meta
    	Assets/Standard Assets/ParticleSystems/Prefabs/HoseMobile.prefab
    	Assets/Standard Assets/ParticleSystems/Prefabs/HoseMobile.prefab.meta
    	Assets/Standard Assets/ParticleSystems/Prefabs/Smoke.prefab
    	Assets/Standard Assets/ParticleSystems/Prefabs/Smoke.prefab.meta
    	Assets/Standard Assets/ParticleSystems/Prefabs/Steam.prefab
    	Assets/Standard Assets/ParticleSystems/Prefabs/Steam.prefab.meta
    	Assets/Standard Assets/ParticleSystems/Prefabs/SteamMobile.prefab
    	Assets/Standard Assets/ParticleSystems/Prefabs/SteamMobile.prefab.meta
    	Assets/Standard Assets/ParticleSystems/Prefabs/TyreBurnoutSmoke.prefab
    	Assets/Standard
    Assets/ParticleSystems/Prefabs/TyreBurnoutSmoke.prefab.meta
    	Assets/Standard Assets/ParticleSystems/Prefabs/WildFire.prefab
    	Assets/Standard Assets/ParticleSystems/Prefabs/WildFire.prefab.meta
    	ProjectSettings/EditorBuildSettings.asset
    
    merge

---
+++ Assets/_Scenes/Playable Build/InventoryEscape_Build.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryEscape_Build : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.LoadLevel(Build_Scenes.MainScene);
+		}
+	}
+}
