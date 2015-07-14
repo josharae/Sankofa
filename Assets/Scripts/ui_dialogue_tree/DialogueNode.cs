eb412df08118d9e6f9b69f446b88611eb2c78fed
Shivam-Shah <mathwizard98@gmail.com>
2015-07-13 20:36:56 -0700

    ui_dialogueTree
    This is the beginning of me learning how to use the dialogue tree

---
+++ Assets/Scripts/ui_dialogue_tree/DialogueNode.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace DialogueTree
+{
+	public class DialogueNode
+	{
+
+		public int NodeID = -1;
+
+		public string Text;
+
+		public List<DialogueOption> Options;
+
+		//parameterless constructor for serialization
+
+		public DialogueNode() 
+		{
+			Options = new List<DialogueOption> ();
+		}
+
+		public DialogueNode(string text)
+		{
+			Text = text;
+			Options = new List<DialogueOption> ();
+		}
+	}
+}
