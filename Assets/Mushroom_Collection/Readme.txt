2086529acd239911cdaa4ed089da8ab8b257d66c
ciansmyth <fluffster999@gmail.com>
2015-07-27 17:32:06 -0700

    Imported some mushroom prefabs

---
+++ Assets/Mushroom_Collection/Readme.txt
@@ -0,0 +1,3 @@
+Version 1.0
+
+A simple package of 3d mushrooms. Pretty easy to use! Just drop the prefabs in your scene.
--- Assets/Scripts/Pathfinding/Grid.cs
+++ Assets/Scripts/Pathfinding/Grid.cs
@@ -34,6 +34,9 @@ public class Grid : MonoBehaviour {
 			for (int y = 0; y < gridSizeY; y ++) {
 				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
 				bool walkable = !(Physics.CheckSphere(worldPoint,nodeRadius,unwalkableMask));
+				if (walkable) {
+					walkable = (worldPoint.y > Terrain.activeTerrain.SampleHeight (worldPoint) + Terrain.activeTerrain.transform.position.y);
+				}
 				grid[x,y] = new Node(walkable,worldPoint, x,y);
 			}
 		}
