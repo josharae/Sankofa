a7a9d7ea018334e11c5c5b921e4d51a71096927d
gfazenda <gludvigf@uci.edu>
2015-08-12 11:51:19 -0700

    separated script to manage spawning

---
+++ Assets/Scripts/Gabriel/SpawnManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnManager : MonoBehaviour {
+	Dictionary<int, List<Vector3>> collectedItems = new Dictionary<int, List<Vector3>>();
+	Dictionary<int, int> itemsToCollect = new Dictionary<int, int>();
+	int currentLevel = 0;
+
+	public void addCollectedItem(Vector3 itemPosition){
+		if (collectedItems.ContainsKey (currentLevel))
+			collectedItems [currentLevel].Add (itemPosition);
+		else{
+			List<Vector3> newList = new List<Vector3>();
+			newList.Add(itemPosition);
+			collectedItems.Add (currentLevel,newList);
+		}
+		itemsToCollect [currentLevel] -= 1;
+	}
+
+	public void checkCollectedItems(int level){
+		currentLevel = level;
+		GameObject[] collectibles = GameObject.FindGameObjectsWithTag (Tags.Collectible);
+		if (collectedItems.ContainsKey (currentLevel) && itemsToCollect.ContainsKey(currentLevel)){
+			if(itemsToCollect[currentLevel] > 0) {
+				for (int i =0; i<collectibles.Length; i++) {
+					for (int x =0; x<collectedItems[currentLevel].Count; x++) {
+						if (Vector3.Distance (collectibles [i].transform.position, collectedItems [currentLevel] [x]) < 5) {
+							collectibles [i].SetActive (false);
+						}
+					}
+				}
+			}
+		} else
+			itemsToCollect.Add (currentLevel, collectibles.Length);
+	}
+}
--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -1,13 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class GameManagerScript : MonoBehaviour {
 
 	bool isPaused = false, showingInventory = false;
-	Dictionary<int, List<Vector3>> collectedItems = new Dictionary<int, List<Vector3>>();
-	Dictionary<int, int> itemsToCollect = new Dictionary<int, int>();
+
 	public Text marbleText, maskText;
 	int marbles =0, masks= 0, currentLevel;
 	GameObject[] InGameButtons;
@@ -19,28 +17,10 @@ public class GameManagerScript : MonoBehaviour {
 	}
 
 	void OnLevelWasLoaded(int level) {
-		currentLevel = level;
-		checkCollectedItems ();
+		this.GetComponent<SpawnManager> ().checkCollectedItems (level);
 	}
 
-	void checkCollectedItems(){
-		GameObject[] collectibles = GameObject.FindGameObjectsWithTag (Tags.Collectible);
-		if (collectedItems.ContainsKey (currentLevel) && itemsToCollect.ContainsKey(currentLevel)){
-		    if(itemsToCollect[currentLevel] < collectibles.Length) {
-				Debug.Log(collectibles.Length);
-				Debug.Log(collectedItems[currentLevel].Count);
-			for (int i =0; i<collectibles.Length; i++) {
-				for (int x =0; x<collectedItems[currentLevel].Count; x++) {
-					Debug.Log(collectibles [i].transform.position + "  " + collectedItems [currentLevel] [x]);
-					if (Vector3.Distance (collectibles [i].transform.position, collectedItems [currentLevel] [x]) < 5) {
-						collectibles [i].SetActive (false);
-					}
-				}
-			}
-		}
-		} else
-			itemsToCollect.Add (currentLevel, collectibles.Length);
-	}
+
 	
 	// Update is called once per frame
 	void Update () {
@@ -70,14 +50,8 @@ public class GameManagerScript : MonoBehaviour {
 		else
 			masks += 1;
 
-		if (collectedItems.ContainsKey (currentLevel))
-			collectedItems [currentLevel].Add (itemPosition);
-		else{
-			List<Vector3> newList = new List<Vector3>();
-			newList.Add(itemPosition);
-			collectedItems.Add (currentLevel,newList);
-		}
-		itemsToCollect [currentLevel] -= 1;
+		this.GetComponent<SpawnManager> ().addCollectedItem (itemPosition);
+
 		setItemsText ();
 	}
 
