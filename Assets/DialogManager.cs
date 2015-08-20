feebf2adc7f7e9d5793a5d1819d0c2608497b123
gfazenda <gludvigf@uci.edu>
2015-08-19 21:03:11 -0700

    dialog system done

---
+++ Assets/DialogManager.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.IO; 
+
+public class DialogManager : MonoBehaviour {
+	[SerializeField] Button dialogButton;
+	[SerializeField] float textSpeed = 0.1f;
+	
+	GameObject textObject, player;
+	Text textField;
+	List <string> dialogue = new List<string>();
+	
+	int currentIndex = 0, phraseIndex = 0;
+	string defaultText;
+	bool isFinished = false, dialogStarted = false;
+	
+	void Start () {
+		textObject = GameObject.FindGameObjectWithTag (Tags.subtitles);
+		player = GameObject.FindGameObjectWithTag (Tags.Player);
+		textField = textObject.GetComponent<Text> ();
+	}
+	
+	public void LoadandStartDialog(string fileName, string objName){
+		string line;
+		defaultText = objName + ":\n";
+		StreamReader theReader = new StreamReader("Subtitles\\"+fileName);
+		
+		do {
+			line = theReader.ReadLine ();
+			if(line != null && line.Length != 0)
+				dialogue.Add (line);		
+		} while (line != null);
+		
+		theReader.Close();	
+		startDialog ();
+	}
+	
+	public void nextPhrase(){
+		if (dialogStarted) {
+			CancelInvoke ("showSubtitle");
+			if (phraseIndex < dialogue [currentIndex].Length) {
+				textField.text = defaultText + dialogue [currentIndex];
+				phraseIndex = dialogue [currentIndex].Length;
+				if(currentIndex == dialogue.Count - 1)
+					buttonText(true);
+			} else if (currentIndex < dialogue.Count - 1) {
+				resetVariables ();
+				currentIndex += 1;
+				startDialog();
+			} else {
+				finishDialog ();
+			}				
+		}
+	}
+	
+	
+	public void finishDialog(){
+		resetVariables();
+		isFinished = true;
+		defaultText = "";
+		currentIndex = 0;
+		dialogStarted = false;
+		dialogButton.gameObject.SetActive (false);
+	}
+	
+	void resetVariables(){
+		phraseIndex = 0;
+		textField.text = "";
+	}
+	
+	public bool isDialogFinished(){
+		return isFinished;
+	}
+	
+	void startDialog(){ //works both to start a new dialog or go to next phrase
+		dialogStarted = true;
+		textField.text = defaultText;
+		dialogButton.gameObject.SetActive (true);
+		buttonText ();
+		InvokeRepeating ("showSubtitle", 0f, textSpeed);
+	}
+
+	void buttonText(bool finish = false){
+		string buttonText = "";
+		if (finish)
+			buttonText = "Close";
+		else 
+			buttonText = "Next";
+
+		dialogButton.GetComponentInChildren<Text> ().text = buttonText;
+	}
+	
+	public void closeDialog(){
+		textField.text = "";
+		isFinished = true;
+	}
+	
+	void showSubtitle(){
+		isFinished = false;
+		if (phraseIndex < dialogue [currentIndex].Length) {
+			textField.text += dialogue [currentIndex] [phraseIndex];
+			phraseIndex += 1;
+		} else {
+			if(currentIndex == dialogue.Count - 1)
+				buttonText(true);
+			CancelInvoke ("showSubtitle");
+			phraseIndex = 0;
+		}
+	}
+}
--- Assets/Scripts/Gabriel/SubtitleScript.cs
+++ Assets/Scripts/Gabriel/SubtitleScript.cs
@@ -5,86 +5,17 @@ using UnityEngine.UI;
 using System.IO; 
 
 public class SubtitleScript : MonoBehaviour {
-	[SerializeField] string textFile;
-	GameObject textObject;
-	Text textField;
-	List <string> dialogue = new List<string>();
-	int currentIndex = 0, phraseIndex = 0;
-	bool isFinished = false, dialogStarted = false;
-	float textSpeed = 0.1f;
-	// Use this for initialization
+	GameObject GameManager, player;
+	[SerializeField] string textFileName;
+
 	void Start () {
-		textObject = GameObject.FindGameObjectWithTag (Tags.subtitles);
-		textField = textObject.GetComponent<Text> ();
-		Load (textFile);
+		GameManager = GameObject.Find (ItemNames.GameManager);
+		player = GameObject.FindGameObjectWithTag (Tags.Player);
 	}
 
-	private void Load(string fileName){
-			string line;
-			StreamReader theReader = new StreamReader("Subtitles\\"+fileName);
-
-			do {
-				line = theReader.ReadLine ();
-				if(line != null && line.Length != 0)
-					dialogue.Add (line);		
-			} while (line != null);
-		 
-			theReader.Close();	
-		}
-
-		public void nextPhrase(){
-			if (dialogStarted) {
-				CancelInvoke ("showSubtitle");
-				if (phraseIndex < dialogue [currentIndex].Length) {
-					textField.text = dialogue [currentIndex];
-					phraseIndex = dialogue [currentIndex].Length;
-				} else if (currentIndex < dialogue.Count - 1) {
-					resetVariables ();
-					currentIndex += 1;
-					InvokeRepeating ("showSubtitle", 0f, textSpeed);
-				} else {
-					finishDialog ();
-				}				
-			}
-		}
-		
-		
-		void finishDialog(){
-			resetVariables();
-			isFinished = true;
-			currentIndex = 0;
-			dialogStarted = false;
-		}
-
-		void resetVariables(){
-			phraseIndex = 0;
-			textField.text = "";
-		}
-
-		public bool isDialogFinished(){
-			return isFinished;
-		}
-
-		public void closeDialog(){
-			textField.text = "";
-			isFinished = true;
-		}
-
-		void showSubtitle(){
-			isFinished = false;
-			if (phraseIndex < dialogue [currentIndex].Length) {
-				textField.text += dialogue [currentIndex] [phraseIndex];
-				phraseIndex += 1;
-			} else {
-				CancelInvoke ("showSubtitle");
-				phraseIndex = 0;
-			}
-		}
-
-		void OnMouseDown(){
-			if (!dialogStarted) {
-				dialogStarted = true;
-				InvokeRepeating ("showSubtitle", 0f, textSpeed);
-			}
-		}
+	void OnMouseDown(){
+		if(Vector3.Distance(this.transform.position,player.transform.position) < 10){
+			GameManager.GetComponent<DialogManager>().LoadandStartDialog(textFileName,this.name);		
+		}	
+	}
 }
